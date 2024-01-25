e1270c03e2256a29713ccfeb241bb777cb65f78f
BrenoMoura00 <brenoluisadv@gmail.com>
2024-01-24 22:22:20 -0300

    Criando as 'variaveis' do filme

---
+++ src/Filme.java
@@ -0,0 +1,10 @@
+public class Filme {
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    double somaDasAvaliacoes;
+    int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+
+
+}
