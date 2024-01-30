f872f30facaa2927fe1f9fdee40e9e9480013d5f
BrenoMoura00 <brenoluisadv@gmail.com>
2024-01-29 21:58:43 -0300

    Criando a classe Serie e colocando todos os atributos e getters e setters

--- src/br/com/alura/cinemoovie/modelos/Filme.java
+++ src/br/com/alura/cinemoovie/modelos/Filme.java
@@ -1,5 +1,13 @@
 package br.com.alura.cinemoovie.modelos;
 
-public class Filme {
+public class Filme extends Titulo{
+        private String diretor;
 
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
 }
