33d6835af5d502bb6d1b9b2cc3ec766e043cd35a
BrenoMoura00 <brenoluisadv@gmail.com>
2024-01-31 21:48:04 -0300

    Criando o primeiro Array para armazenar filmes

--- src/br/com/alura/cinemoovie/Principal.java
+++ src/br/com/alura/cinemoovie/Principal.java
@@ -6,6 +6,8 @@ import br.com.alura.cinemoovie.modelos.Episodio;
 import br.com.alura.cinemoovie.modelos.Filme;
 import br.com.alura.cinemoovie.modelos.Serie;
 
+import java.util.ArrayList;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
@@ -51,5 +53,18 @@ public class Principal {
         episodio.setTotalVisualizacoes(300);
         System.out.println(episodio.getSerie());
         filtro.filtra(episodio);
+
+        Filme filmeBreno = new Filme();
+        filmeBreno.setNome("Dogville");
+        filmeBreno.setDuracaoEmMinutos(200);
+        filmeBreno.setAnoDeLancamento(2003);
+        filmeBreno.avalia(10);
+
+        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+        listaDeFilmes.add(meuFilme);
+        listaDeFilmes.add(filme2);
+        listaDeFilmes.add(filmeBreno);
+        System.out.println("Tamanho da lista: " + listaDeFilmes.size());
+        System.out.println("Primeiro Filme: " + listaDeFilmes.get(0).getNome());
     }
 }
