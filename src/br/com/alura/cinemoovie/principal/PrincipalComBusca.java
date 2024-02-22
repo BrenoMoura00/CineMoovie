f5bee8573a3d547b1f0fb09a47eb6106efda1d28
BrenoMoura00 <brenoluisadv@gmail.com>
2024-02-22 16:09:27 -0300

    Criando metodo compareTo

--- src/br/com/alura/cinemoovie/principal/PrincipalComBusca.java
+++ src/br/com/alura/cinemoovie/principal/PrincipalComBusca.java
@@ -1,21 +1,36 @@
 package br.com.alura.cinemoovie.principal;
 
+import br.com.alura.cinemoovie.modelos.Titulo;
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("digite um filme para busca: ");
+        var busca = scanner.nextLine();
+
+        String endereco = "https://www.omdbapi.com/?t="+ busca +"&apikey=d1f51703";
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=avatar+2&apikey=d1f51703"))
+                .uri(URI.create(endereco))
                 .build();
 
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+        String json = response.body();
+        System.out.println(json);
+
+
+        Gson gson = new Gson();
+        Titulo meuTitulo = gson.fromJson(json, Titulo.class);
+        System.out.println(meuTitulo);
     }
 }
